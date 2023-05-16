bd64eaa288ff5ade4c989429bde32f2df3f3ea7d
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-05-16 15:17:27 +0700

    Operator Aritmatika

---
+++ operatorArtimatika.php
@@ -0,0 +1,26 @@
+<?php 
+
+$a = 10;
+$b = 15;
+
+$result = $a + $b; // Penjumlahan
+var_dump($result);
+echo $result;
+
+$result = $a - $b; // Pengurangan
+var_dump($result);
+echo $result;
+
+$resultPositif = +$result; // Positif
+var_dump($resultPositif);
+echo $resultPositif;
+
+$resultNegatif = -$result; // Negatif
+var_dump($resultNegatif);
+echo $resultNegatif;
+
+$resultModulo = 100 % 3; // Perpangkatan Modulo
+var_dump($resultModulo);
+echo $resultModulo;
+
+?>
