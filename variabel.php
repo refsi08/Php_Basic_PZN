1534c1fb08d538ea5679666043e37f9dcae52324
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-06-07 15:47:22 +0700

    perbaikan variabel

--- variabel.php
+++ variabel.php
@@ -7,4 +7,16 @@ $umur = 24;
 echo "Nama Lengkap : $namaLengkap";
 echo "\n";
 echo "Umur : $umur";
+
+$contoh = "Eko";
+$$contoh = "Kurniawan";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "eko : ";
+echo $eko;
+echo "\n";
+
 ?>
