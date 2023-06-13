0d39a3b69115b33bf8c13ce0533ccd751f8a5417
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-06-13 16:16:27 +0700

    expression statement dan block

---
+++ expression_statement_block.php
@@ -0,0 +1,31 @@
+/**
+    - Expression
+
+    $a = 5;
+    $b = $a;
+
+    - Expression complex
+    function getValue(){
+    return 100;
+    }
+    $value = getValue();
+
+
+    - Statement
+
+    $name = "Eko Kurniawan Khanndey";
+
+    echo $name;
+
+    $date = new DateTime();
+
+    - Block
+
+    function runApp($name)
+    {
+    echo "Start Program" . PHP_EOL;
+    echo "Hello $name" . PHP_EOL;
+    echo "End Program" . PHP_EOL;
+    }
+
+*/
