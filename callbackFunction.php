2533c74212522910d0867e6b1ec9e044b5919e3b
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-06-19 09:55:23 +0700

    perbaikan callback function

--- callbackFunction.php
+++ callbackFunction.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * menggunakan kata kunci callable adalah cara lain untuk membuat argument function
+ * dipanggil menggunakan call_user_func()
+ */
 function sayHello(string $name, callable $filter)
 {
     $finalName = call_user_func($filter, $name);
